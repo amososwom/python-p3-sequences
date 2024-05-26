cea4efdd189c1fd5d9b8b7fe70540e5ee5169b98
primeboogie <primemarkboogie@gmail.com>
2024-05-26 06:42:55 -0100

    completed sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0 or len(str(length)) == 0:
+        print([])
+        return
+    
+    result = []
+    a, b = 0, 1
+    for i in range(length):
+        result.append(a)
+        a, b = b, b+a
+
+    print(result)
+   
+print_fibonacci(0)
